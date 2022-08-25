26bb21db3d41f42c0a36d95fcd2502ee6339272e
LunaDeLaet <luna.dl@hotmail.com>
2022-08-25 15:02:27 +0200

    add POST intercept

--- Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
+++ Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
@@ -27,6 +27,10 @@ describe('intercept petstore with cypress', () => {
     cy.intercept('POST', '/v2/pet', { fixture: 'pet.json' }).as('pet')
     cy.get('button[class="btn execute opblock-control__btn"').click()
 
-    cy.wait('@pet')
+    cy.wait('@pet').then((interception) => {
+      // log response data
+      cy.log(JSON.stringify(interception))
+      console.log(JSON.stringify(interception))
+    })
   })
 })
