06533a375ffb265e729e27315c39f1644ea6c75e
LunaDeLaet <luna.dl@hotmail.com>
2022-08-25 14:29:16 +0200

    edit

--- Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
+++ Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
@@ -3,18 +3,17 @@
 describe('intercept petstore with cypress', () => {
   it('test api with simple intercept stubbing', () => {
     cy.visit('https://petstore.swagger.io')
-    cy.intercept('GET', '/pet/findByStatus', { status: 'available' }).as('pet')
 
     // when execute link gets clicked, pet api is getting called
     cy.get("[id='operations-pet-findPetsByStatus']").click()
     cy.get('button[class="btn try-out__btn"').click()
     cy.get('option[value="available"').click()
+    cy.intercept('GET', '/v2/pet/findByStatus*', { status: 'available' }).as(
+      'pet'
+    )
     cy.get('button[class="btn execute opblock-control__btn"').click()
 
     // spy
-    cy.wait('@pet').then((interception) => {
-      // log response data
-      cy.log(JSON.stringify(interception))
-    })
+    cy.wait('@pet')
   })
 })
