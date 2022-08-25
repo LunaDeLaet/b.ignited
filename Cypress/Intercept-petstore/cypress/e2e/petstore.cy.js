5225554afddbdb48820e98752a69875faac40343
LunaDeLaet <luna.dl@hotmail.com>
2022-08-25 16:57:10 +0200

    add response validation

--- Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
+++ Cypress/Intercept-petstore/cypress/e2e/petstore.cy.js
@@ -8,20 +8,21 @@ describe('intercept petstore with cypress', () => {
     cy.get("[id='operations-pet-findPetsByStatus']").click()
     cy.get('button[class="btn try-out__btn"').click()
     cy.get('td[class="parameters-col_description"] select').select('available')
-    cy.intercept('GET', '/v2/pet/findByStatus?status=available').as('pet')
+    cy.intercept('GET', '/v2/pet/findByStatus?status=available', {
+      name: 'doggie',
+    }).as('pet')
     cy.get('button[class="btn execute opblock-control__btn"').click()
 
     // spy
     cy.wait('@pet').then((interception) => {
       // log response data
       cy.log(JSON.stringify(interception.response.body))
-      console.log(JSON.stringify(interception.response.body))
+      console.log(interception)
     })
   })
 
   it('mocking with intercept test with dynamic fixture', () => {
     cy.visit('https://petstore.swagger.io')
-    // when execute link gets clicked, pet api is getting called
     cy.get("[id='operations-pet-addPet']").click()
     cy.get('button[class="btn try-out__btn"').click()
     cy.intercept('POST', '/v2/pet', { fixture: 'pet.json' }).as('pet')
@@ -30,7 +31,16 @@ describe('intercept petstore with cypress', () => {
     cy.wait('@pet').then((interception) => {
       // log response data
       cy.log(JSON.stringify(interception.response.body))
-      console.log(JSON.stringify(interception.response.body))
+      console.log(interception)
+      // validate response
+      expect(interception.response.body).to.have.all.keys(
+        'category',
+        'id',
+        'name',
+        'photoUrls',
+        'status',
+        'tags'
+      )
     })
   })
 })
