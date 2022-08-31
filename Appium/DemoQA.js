adcbccc2bcf7275c8bb53b61e252c6c4a420ee7b
LunaDeLaet <luna.dl@hotmail.com>
2022-08-31 18:54:51 +0200

    add demo qa appium

--- Appium/DemoQA.js
+++ Appium/DemoQA.js
@@ -19,17 +19,22 @@ async function main() {
 
   // type into a text field and check that the correct text was entered
   await browser.url('https://demoqa.com/')
-  await browser.url('https://demoqa.com/')
+
+  formsPage = await browser.$("(//div[contains(@class, 'top-card')])[2]")
+  await browser.execute('arguments[0].click();', formsPage)
+
+  practiceForm = await browser.$('.text=Practice Form')
+  await browser.execute('arguments[0].click();', practiceForm)
 
   //locate first name input
-  inputFirstName = await browser.$("(//input[@id, 'firstName'])")
+  inputFirstName = await browser.$("//input[@id='firstName']")
   // send keys
-  await inputFirstName.type('Luna')
+  await inputFirstName.setValue('Luna')
 
   //locate last name input
-  inputFirstName = await browser.$("(//input[@id, 'lastName'])")
+  inputFirstName = await browser.$("//input[@id='lastName']")
   // send keys
-  await inputFirstName.type('De Laet')
+  await inputFirstName.setValue('De Laet')
 
   //locate gender female radio button
   genderFemale = await browser.$("//label[@for='gender-radio-2']")
@@ -38,13 +43,13 @@ async function main() {
   // locate mobile number input
   userNumber = await browser.$("//input[@id='userNumber']")
   // send keys
-  await userNumber.type('0467039777')
+  await userNumber.setValue('0467039777')
 
   //  submit
   submit = await browser.$("//button[@id='submit']")
   await browser.execute('arguments[0].click();', submit)
 
-  await browser.deleteSession()
+  //await browser.deleteSession()
 }
 
 main()
